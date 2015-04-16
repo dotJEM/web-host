9896ce24329b9399824e9a857ca6dc735d7b6c90
Jens Melgaard <admin@it-links.dk>
2015-04-16 08:39:38 +0200

    Removed excessive line breaks.

--- DotJEM.Web.Host/Providers/Services/ContentService.cs
+++ DotJEM.Web.Host/Providers/Services/ContentService.cs
@@ -40,10 +40,11 @@ namespace DotJEM.Web.Host.Providers.Services
 
         public IEnumerable<JObject> Get(string contentType, int skip = 0, int take = 20)
         {
-            var res = index.Search("contentType: " + contentType)
+            JObject[] res = index.Search("contentType: " + contentType)
                 .Skip(skip).Take(take)
                 .Select(hit => hit.Json)
-                .Select(json => pipeline.ExecuteAfterGet(json, contentType)) //Executes the pipeline for each element found
+                //Note: Execute the pipeline for each element found
+                .Select(json => pipeline.ExecuteAfterGet(json, contentType))
                 .Cast<JObject>().ToArray();
 
             return res;
@@ -51,60 +52,42 @@ namespace DotJEM.Web.Host.Providers.Services
             //TODO: Execute pipeline for array
             //TODO: Paging and other neat stuff...
             //TODO: Use search for optimized performance!...
-            //return area.Get(contentType).Skip(skip).Take(take);
         }
 
         public JObject Get(Guid id, string contentType)
         {
             //TODO: Use search for optimized performance!...
-            JObject entity = area.Get(id);
-
             //TODO: Throw exception if not found?
-            //if (entity == null)
-            //    return Request.CreateResponse(HttpStatusCode.NotFound, "Could not find cotent of type '" + contentType + "' with id [" + id + "] in area '" + Area.Name + "'");
-
+            JObject entity = area.Get(id);
             return pipeline.ExecuteAfterGet(entity, contentType);
         }
 
         public JObject Post(string contentType, JObject entity)
         {
             entity = pipeline.ExecuteBeforePost(entity, contentType);
-
             entity = area.Insert(contentType, entity);
-
             entity = pipeline.ExecuteAfterPost(entity, contentType);
-
             manager.QueueUpdate(entity);
-
-            //index.Write(entity);
             return entity;
         }
 
         public JObject Put(Guid id, string contentType, JObject entity)
         {
             entity = pipeline.ExecuteBeforePut(entity, contentType);
-
             entity = area.Update(id, entity);
-
             entity = pipeline.ExecuteAfterPut(entity, contentType);
-
             manager.QueueUpdate(entity);
-            //index.Write(entity);
             return entity;
         }
 
         public JObject Delete(Guid id, string contentType)
         {
             JObject deleted = area.Delete(id);
+            //TODO: Throw exception if not found?
             if (deleted == null)
                 return null;
 
-            //TODO: Throw exception if not found?
-            //    if (deleted == null)
-            //        return Request.CreateResponse(HttpStatusCode.NotFound, "Could not delete cotent with id [" + id + "] in area '" + Area.Name + "' as it could not be found.");
             manager.QueueDelete(deleted);
-
-            //index.Delete(deleted);
             return pipeline.ExecuteAfterDelete(deleted, contentType);
         }
     }
