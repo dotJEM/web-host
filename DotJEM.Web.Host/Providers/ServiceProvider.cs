a6d83a3a9388a9943e062c24b5bccf114a13aa4a
Jens Melgaard <admin@it-links.dk>
2015-08-27 11:19:01 +0200

    Switching to Concurrent dictionary

--- DotJEM.Web.Host/Providers/ServiceProvider.cs
+++ DotJEM.Web.Host/Providers/ServiceProvider.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using Castle.Windsor;
 
 namespace DotJEM.Web.Host.Providers
@@ -13,7 +14,8 @@ namespace DotJEM.Web.Host.Providers
     public abstract class ServiceProvider<TService> : IServiceProvider<TService>
     {
         private readonly Func<string, TService> factory;
-        private readonly Dictionary<string, TService> services = new Dictionary<string, TService>();
+        //private readonly Dictionary<string, TService> services = new Dictionary<string, TService>();
+        private readonly ConcurrentDictionary<string, TService> services = new ConcurrentDictionary<string, TService>();
 
         protected ServiceProvider(Func<string, TService> factory)
         {
@@ -29,12 +31,24 @@ namespace DotJEM.Web.Host.Providers
 
         private TService GetOrCreateService(string areaName)
         {
-            return !services.ContainsKey(areaName) ? (services[areaName] = factory(areaName)) : services[areaName];
+            try
+            {
+                return services.GetOrAdd(areaName, factory);
+
+                //return !services.ContainsKey(areaName) ? (services[areaName] = factory(areaName)) : services[areaName];
+            }
+            catch (NullReferenceException ex)
+            {
+                string message = areaName != null ? "Area name was: " + areaName : "Area name was NULL: ";
+                throw new NullReferenceException(message, ex);
+            }
+
         }
 
         public virtual bool Release(string areaName)
         {
-            return services.Remove(areaName);
+            TService removed;
+            return services.TryRemove(areaName, out removed);
         }
     }
 }
