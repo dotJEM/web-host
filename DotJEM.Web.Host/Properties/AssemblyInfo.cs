161fda06cdc78fa623bdbfdb5b34b4b62d2a2659
Jens Melgaard <admin@it-links.dk>
2016-05-02 15:10:00 +0200

    Update AssemblyInfo.cs

--- DotJEM.Web.Host/Properties/AssemblyInfo.cs
+++ DotJEM.Web.Host/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("DotJEM.Web.Host")]
 [assembly: AssemblyCompany("N/A")]
-[assembly: AssemblyCopyright("Copyright © DotJEM A/S 2014-2015")]
+[assembly: AssemblyCopyright("Copyright © DotJEM 014-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
