b55dcb78676753ff393fd0070d335a8d70057917
Jens Melgaard <jmd@systematic.com>
2019-05-08 16:40:23 +0200

    Debugging why the IExceptionHandler suddenly stopped working.

--- Demo/Controllers/IndexController.cs
+++ Demo/Controllers/IndexController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using DotJEM.Web.Host;
+using DotJEM.Web.Host.Providers.Services.DiffMerge;
+using Newtonsoft.Json.Linq;
 
 namespace Demo.Controllers
 {
@@ -7,6 +11,14 @@ namespace Demo.Controllers
         public ActionResult Get()
         {
                 return View("~/Index.cshtml");
+        }
+    }
+
+    public class ExceptionController : ApiController
+    {
+        public string Get()
+        {
+            throw new JsonMergeConflictException(new MergeResult(true, new JObject(), new JObject(), new JObject(), new JObject()));
         }
     }
 }
--- Demo/Global.asax.cs
+++ Demo/Global.asax.cs
@@ -1,4 +1,13 @@
-﻿using System.Web;
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
 using Demo.Controllers;
 using DotJEM.Web.Host;
 using DotJEM.Web.Host.Configuration;
@@ -9,15 +18,39 @@ namespace Demo
     {
         protected void Application_Start()
         {
-            new DemoHost().Start();
+            new DemoHost(GlobalConfiguration.Configuration).Start();
+            GlobalConfiguration.Configuration.Services.Replace(typeof(IExceptionHandler), new MyExceptionHandler());
+        }
+    }
+
+    public class MyExceptionHandler : System.Web.Http.ExceptionHandling.ExceptionHandler
+    {
+        public override Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                HttpResponseMessage message = context.Request.CreateResponse(HttpStatusCode.Conflict, context.Exception.GetType().FullName);
+                context.Result = new ResponseMessageResult(message);
+            }, cancellationToken);
+
         }
     }
 
     public class DemoHost : WebHost
     {
+        public DemoHost(HttpConfiguration configuration) : base(configuration)
+        {
+        }
+
         protected override void Configure(IRouter router)
         {
+            router.Route("api/exception").To<ExceptionController>();
             router.Default().To<IndexController>();
+        }
+
+        protected override void Configure(IWindsorContainer container)
+        {
+            container.Register(Component.For<ExceptionController>().LifestyleTransient());
         }
     }
 }
