a6c1723c1dc466484dac3f9dc589eaad1ca8c8c3
Jens Melgaard <admin@it-links.dk>
2021-09-23 21:07:32 +0200

    Added specific filters for HttpMethods etc.

--- src/DotJEM.Web.Host/Providers/AsyncPipeline/Attributes/HttpMethodFilterAttribute.cs
+++ src/DotJEM.Web.Host/Providers/AsyncPipeline/Attributes/HttpMethodFilterAttribute.cs
@@ -6,10 +6,59 @@ namespace DotJEM.Web.Host.Providers.AsyncPipeline.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
     public class HttpMethodFilterAttribute : PropertyFilterAttribute
     {
-
-        public HttpMethodFilterAttribute(string regex, RegexOptions options = RegexOptions.None)
-            : base("method", regex, options | RegexOptions.IgnoreCase)
+        public HttpMethodFilterAttribute(string method, RegexOptions options = RegexOptions.None)
+            : base("method", $"^{method}$", options | RegexOptions.IgnoreCase)
         {
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class HttpGetMethodFilterAttribute : HttpMethodFilterAttribute
+    {
+        public HttpGetMethodFilterAttribute(RegexOptions options = RegexOptions.None)
+            : base("GET", options)
+        {
+
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class HttpPostMethodFilterAttribute : HttpMethodFilterAttribute
+    {
+        public HttpPostMethodFilterAttribute(RegexOptions options = RegexOptions.None)
+            : base("POST", options)
+        {
+
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class HttpPutMethodFilterAttribute : HttpMethodFilterAttribute
+    {
+        public HttpPutMethodFilterAttribute(RegexOptions options = RegexOptions.None)
+            : base("PUT", options)
+        {
+
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class HttpPatchMethodFilterAttribute : HttpMethodFilterAttribute
+    {
+        public HttpPatchMethodFilterAttribute(RegexOptions options = RegexOptions.None)
+            : base("PATCH", options)
+        {
+
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class HttpDeleteMethodFilterAttribute : HttpMethodFilterAttribute
+    {
+        public HttpDeleteMethodFilterAttribute(RegexOptions options = RegexOptions.None)
+            : base("DELETE", options)
+        {
+
         }
     }
 }
