4f668bd4b7845a0cea42fc079bdb2fe8a74a90e3
jmd <jmd@Systematic.com>
2024-02-12 09:44:25 +0100

    Snapshot schemas as well.

---
+++ src/DotJEM.Web.Host/Providers/Data/Index/Snapshots/SnapshotManager.cs
@@ -0,0 +1,156 @@
+﻿using DotJEM.Json.Index2.Management.Snapshots;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotJEM.Json.Index2.Management.Info;
+using DotJEM.Json.Index2.Management.Tracking;
+using DotJEM.ObservableExtensions.InfoStreams;
+using DotJEM.Web.Host.Providers.Data.Index.Schemas;
+using DotJEM.Json.Index2.Snapshots;
+using DotJEM.Json.Index2;
+using DotJEM.Web.Scheduler;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+using System.Web.UI.WebControls;
+
+namespace DotJEM.Web.Host.Providers.Data.Index.Snapshots;
+
+public class WebHostJsonIndexSnapshotManager : IJsonIndexSnapshotManager
+{
+    private readonly IJsonIndex index;
+    private readonly ISnapshotStrategy strategy;
+    private readonly IWebTaskScheduler scheduler;
+    private readonly IInfoStream<JsonIndexSnapshotManager> infoStream = new InfoStream<JsonIndexSnapshotManager>();
+    private readonly ISchemaCollection schemas;
+
+    private readonly string schedule;
+
+    public IInfoStream InfoStream => infoStream;
+
+    public WebHostJsonIndexSnapshotManager(IJsonIndex index, ISnapshotStrategy snapshotStrategy, IWebTaskScheduler scheduler, ISchemaCollection schemas, string schedule)
+    {
+        this.index = index;
+
+        this.scheduler = scheduler;
+        this.schedule = schedule;
+        this.schemas = schemas;
+        this.strategy = snapshotStrategy;
+        this.strategy.InfoStream.Subscribe(infoStream);
+    }
+
+    public async Task RunAsync(IIngestProgressTracker tracker, bool restoredFromSnapshot)
+    {
+        await tracker.WhenState(IngestInitializationState.Initialized).ConfigureAwait(false);
+        if (!restoredFromSnapshot)
+        {
+            infoStream.WriteInfo("Taking snapshot after initialization.");
+            await TakeSnapshotAsync(tracker.IngestState).ConfigureAwait(false);
+        }
+        scheduler.Schedule(nameof(JsonIndexSnapshotManager), _ => this.TakeSnapshotAsync(tracker.IngestState), schedule);
+    }
+
+    public async Task<bool> TakeSnapshotAsync(StorageIngestState state)
+    {
+        try
+        {
+            JObject json = JObject.FromObject(state);
+            ISnapshotStorage target = strategy.Storage;
+
+            index.Commit();
+            ISnapshot snapshot = await index.TakeSnapshotAsync(target).ConfigureAwait(false);
+            using (ISnapshotWriter writer = snapshot.OpenWriter())
+            {
+                using (JsonTextWriter wr = new(new StreamWriter(writer.OpenStream("manifest.json"))))
+                {
+                    await json.WriteToAsync(wr).ConfigureAwait(false);
+                    await wr.FlushAsync().ConfigureAwait(false);
+                }
+
+                foreach (JSchema jSchema in schemas)
+                {
+                    using JsonTextWriter wr = new(new StreamWriter(writer.OpenStream($"schemas/{jSchema.ContentType}.json")));
+                    await jSchema.Serialize("").WriteToAsync(wr).ConfigureAwait(false);
+                    await wr.FlushAsync().ConfigureAwait(false);
+                }
+            }
+
+            infoStream.WriteSnapshotCreatedEvent(snapshot, "Snapshot created.");
+            return true;
+        }
+        catch (Exception exception)
+        {
+            infoStream.WriteError("Failed to take snapshot.", exception);
+            return false;
+        }
+        finally
+        {
+            strategy.CleanOldSnapshots();
+        }
+    }
+
+    public async Task<RestoreSnapshotResult> RestoreSnapshotAsync()
+    {
+        try
+        {
+            ISnapshotStorage source = strategy.Storage;
+            if (source == null)
+            {
+                infoStream.WriteInfo($"No snapshots found to restore");
+                return new RestoreSnapshotResult(false, default);
+            }
+
+            int count = 0;
+            foreach (ISnapshot snapshot in source.LoadSnapshots())
+            {
+                count++;
+                try
+                {
+                    if (snapshot.Verify() && await index.RestoreSnapshotAsync(snapshot).ConfigureAwait(false))
+                    {
+                        using ISnapshotReader reader = snapshot.OpenReader();
+                        using Stream manifestStream = reader.OpenStream("manifest.json");
+                        JObject manifest = await JObject.LoadAsync(new JsonTextReader(new StreamReader(manifestStream)))
+                            .ConfigureAwait(false);
+                        if (manifest["Areas"] is not JArray areas) continue;
+                        foreach (string schemaPath in reader.FileNames.Where(name => name.StartsWith("schemas/")))
+                            await LoadSchema(schemaPath, reader, manifestStream);
+                        return new RestoreSnapshotResult(true, new StorageIngestState(areas.ToObject<StorageAreaIngestState[]>()));
+                    }
+
+                    snapshot.Verify();
+                }
+                catch (Exception ex)
+                {
+                    infoStream.WriteError($"Failed to restore snapshot {snapshot}.", ex);
+                    snapshot.Verify();
+                }
+            }
+            infoStream.WriteInfo($"No snapshots restored. {count} was found to be corrupt and was deleted.");
+            return new RestoreSnapshotResult(false, new StorageIngestState());
+        }
+        catch (Exception ex)
+        {
+            infoStream.WriteError("Failed to restore snapshot.", ex);
+            return new RestoreSnapshotResult(false, default);
+        }
+    }
+
+    private async Task LoadSchema(string schemaPath, ISnapshotReader reader, Stream manifestStream)
+    {
+        try
+        {
+            string schemaName = schemaPath.Split('/').Last();
+            using Stream schemaStream = reader.OpenStream(schemaPath);
+            JObject schema = await JObject.LoadAsync(new JsonTextReader(new StreamReader(manifestStream)))
+                .ConfigureAwait(false);
+            schemas.AddOrUpdate(schemaName, schema.ToObject<JSchema>());
+        }
+        catch (Exception ex)
+        {
+            infoStream.WriteError($"Failed to load schema: {schemaPath}.", ex);
+        }
+    }
+}
--- src/DotJEM.Web.Host/Providers/Data/IndexAndStorageInstaller.cs
+++ src/DotJEM.Web.Host/Providers/Data/IndexAndStorageInstaller.cs
@@ -8,13 +8,15 @@ using DotJEM.Json.Index2.Management.Snapshots.Zip;
 using DotJEM.Json.Index2.Management.Source;
 using DotJEM.Json.Index2.Management.Writer;
 using DotJEM.Web.Host.Configuration.Elements;
+using DotJEM.Web.Host.Providers.Data.Index.Schemas;
+using DotJEM.Web.Host.Providers.Data.Index.Snapshots;
 using DotJEM.Web.Host.Providers.Data.Storage;
 using DotJEM.Web.Host.Providers.Data.Storage.Cutoff;
 using DotJEM.Web.Scheduler;
 
 namespace DotJEM.Web.Host.Providers.Data;
 
-public class Installer : IWindsorInstaller
+public class IndexAndStorageInstaller : IWindsorInstaller
 {
     public void Install(IWindsorContainer container, IConfigurationStore store)
     {
@@ -36,11 +38,14 @@ public class Installer : IWindsorInstaller
             IWebTaskScheduler scheduler = kernel.Resolve<IWebTaskScheduler>();
             IJsonIndex index = kernel.Resolve<IJsonIndex>();
             IWebHostConfiguration configuration = kernel.Resolve<IWebHostConfiguration>();
-            return new JsonIndexSnapshotManager(index, snapshotStrategy, scheduler, configuration.Index.Snapshots.Interval);
+            ISchemaCollection schemas = kernel.Resolve<ISchemaCollection>();
+            return new WebHostJsonIndexSnapshotManager(index, snapshotStrategy, scheduler, schemas, configuration.Index.Snapshots.Interval);
         }).LifestyleSingleton());
         container.Register(Component.For<IJsonIndexWriter>().ImplementedBy<JsonIndexWriter>());
         container.Register(Component.For<IJsonDocumentSource>().UsingFactoryMethod(kernel => kernel.Resolve<IJsonStorageManager>().DocumentSource).LifestyleSingleton());
         container.Register(Component.For<IJsonIndexManager>().ImplementedBy<JsonIndexManager>().LifestyleSingleton());
         container.Register(Component.For<IDataStorageManager>().ImplementedBy<DataStorageManager>().LifestyleSingleton());
     }
+
+
 }
